746e8969ea0acbc4b72da22522528ef129ee862f
Relsi Maron <relsi@codeup.com.br>
2019-01-17 23:09:09 -0200

    Delega a geração de arquivos para um módulo especializado

--- deputados.py
+++ deputados.py
@@ -9,29 +9,29 @@ class Deputados:
         Parameters
         ----------
         id : `integer`
-              Número(s) identificador(es) de um deputado, separados por vírgulas.
+        Número(s) identificador(es) de um deputado, separados por vírgulas.
         nome : string
-              Parte do nome  do parlamentar.
+        Parte do nome  do parlamentar.
         idLegislatura : integer
-               Número(s) identificador(es) de uma ou mais legislatura(s) de que os parlamentares tenham participado, separados por vírgulas.
+        Número(s) identificador(es) de uma ou mais legislatura(s) de que os parlamentares tenham participado, separados por vírgulas.
         siglaUf : string
-               Uma ou mais sigla(s) de unidades federativas Se ausente, serão retornados deputados de todos os estados.
+        Uma ou mais sigla(s) de unidades federativas Se ausente, serão retornados deputados de todos os estados.
         siglaPartido : string
-               Uma ou mais sigla(s) de partidos aos quais sejam filiados os deputados. Atenção: partidos diferentes podem usar a mesma sigla em diferentes legislaturas!
+        Uma ou mais sigla(s) de partidos aos quais sejam filiados os deputados. Atenção: partidos diferentes podem usar a mesma sigla em diferentes legislaturas!
         siglaSexo : string
-               Letra que designe o gênero dos parlamentares que se deseja buscar, sendo M para masculino e F para feminino
+        Letra que designe o gênero dos parlamentares que se deseja buscar, sendo M para masculino e F para feminino
         pagina : integer
-               Número da página de resultados, a partir de 1, que se deseja obter com a requisição, contendo o número de itens definido pelo parâmetro itens. Se omitido, assume o valor 1.
+        Número da página de resultados, a partir de 1, que se deseja obter com a requisição, contendo o número de itens definido pelo parâmetro itens. Se omitido, assume o valor 1.
         itens : integer
-               Número máximo de itens na página que se deseja obter com esta requisição.
+        Número máximo de itens na página que se deseja obter com esta requisição.
         dataInicio : string
-               Data de início de um intervalo de tempo, no formato AAAA-MM-DD.
+        Data de início de um intervalo de tempo, no formato AAAA-MM-DD.
         dataFim : string
-               Data de término de um intervalo de tempo, no formato AAAA-MM-DD.
+        Data de término de um intervalo de tempo, no formato AAAA-MM-DD.
         ordem : string
-               O sentido da ordenação: asc para A a Z ou 0 a 9, e desc para Z a A ou 9 a 0. Default ASC
+        O sentido da ordenação: asc para A a Z ou 0 a 9, e desc para Z a A ou 9 a 0. Default ASC
         ordenarPor : string
-               Nome do campo pelo qual a lista deve ser ordenada: id, idLegislatura, nome, siglaUF ou siglaPartido Default nome
+        Nome do campo pelo qual a lista deve ser ordenada: id, idLegislatura, nome, siglaUF ou siglaPartido Default nome
         """
         import requests
 
@@ -49,23 +49,16 @@ class Deputados:
         r = requests.get(url)
         retorno = r.json()
 
-        if tipo == 'html':
-            for d in retorno['dados']:
-                dados = """
-                        <div class="col-xs-18 col-sm-6 col-md-3">
-                        <div class="thumbnail">
-                            <img with="114" heigth="152" src=" """+d['urlFoto']+"""  " alt="">
-                            <div class="caption">
-                                <h4>"""+d['nome']+"""</h4>
-                                <h5>"""+d['siglaPartido']+"""</h5>
-                            </div>
-                        </div>
-                    </div> 
-                """
+        if tipo == 'pdf':
+            pass
+        elif tipo == 'ods':
+            pass
+        elif tipo == 'odf':
+            pass
         else:
             dados = retorno
 
         return dados
-    
+
     def detalhes(self, **kwargs):
         pass
---
+++ relatorios.py
@@ -0,0 +1,13 @@
+class OpenFile:
+    
+    def __init__(self):
+        self.tipo = "odf"
+        self.orientacao = "P"
+        self.tamanho = "A4"
+
+
+class Pdf:
+    
+    def __init__(self):
+        self.orientacao = "P"
+        self.tamanho = "A4"
